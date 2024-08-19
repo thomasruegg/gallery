1982123f2757b572e86d36208feb12ae3ddc6e99
thomasruegg <hello@thomasruegg.com>
2024-08-19 18:41:37 +0200

    database setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
+import { index } from "drizzle-orm/mysql-core";
 import Link from "next/link";
 import { mock } from "node:test";
+import { db } from "~/server/db";
 
 const mockUrls = [
   "https://placehold.co/800?text=Thomas&font=roboto",
@@ -13,12 +15,16 @@ const mockImages = mockUrls.map((url, index) => ({
   url,
 }));
 
-export default function HomePage() {
+export default async function HomePage() {
+
+  const posts = await db.query.posts.findMany();
+
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
+        {posts.map((post) => (<div key={post.id}>{post.name}</div>))}
         {[...mockImages, ...mockImages, ...mockImages].map((image) => (
-          <div key={image.id} className="w-48">
+          <div key={image.id + "-" + index} className="w-48">
             <img src={image.url} />
           </div>
         ))}
