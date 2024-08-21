5354f6bd5682e925ac3441c8621769019ea5f7b1
thomasruegg <hello@thomasruegg.com>
2024-08-21 11:16:18 +0200

    fixed dialog fullscreen, made component for image view

--- src/app/img/[id]/page.tsx
+++ src/app/img/[id]/page.tsx
@@ -1,7 +1,14 @@
-export default function PhotoModal({
+import FullPageImageView from "~/components/full-image-page";
+
+export default function PhotoPage({
     params: { id: photoId },
 }: {
     params: { id: string };
 }) {
-    return <div>{photoId}</div>;
+    const idAsNumber = Number(photoId);
+    if (Number.isNaN(idAsNumber)) throw new Error("Invalid photo ID");
+
+    return (
+        <FullPageImageView id={idAsNumber} />
+    );
 }
