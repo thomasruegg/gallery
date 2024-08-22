71e650585512f7b71204c84516605b5360ab1602
thomasruegg <hello@thomasruegg.com>
2024-08-22 12:59:38 +0200

    ratelimit implemented

--- src/app/_components/simple-upload-button.tsx
+++ src/app/_components/simple-upload-button.tsx
@@ -58,6 +58,11 @@ export function SimpleUploadButton() {
                 }
             )
         },
+        onUploadError(error) {
+            posthog.capture("upload_error", { error });
+            toast.dismiss("upload-begin");
+            toast.error("Upload failed (rate limited)!");
+        },
         onClientUploadComplete() {
             toast.dismiss("upload-begin");
             toast("Upload complete!");
